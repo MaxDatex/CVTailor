37bae82c0be870b9fd5c2b938186553c8ef624ba
MaxDatex <maxforsamp98@gmail.com>
2025-05-22 15:57:10 +0300

    add job description max length validation

--- models/job_description_fields.py
+++ models/job_description_fields.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class JobDescriptionContent(BaseModel):
-    about_company: Optional[str] = ...
-    about_role: Optional[str] = ...
-    requirements: Optional[str] = ...
-    nice_to_have: Optional[str] = ...
-    responsibilities: Optional[str] = ...
-    other: Optional[str] = ...
+    about_company: Optional[str] = Field(None, max_length=2000)
+    about_role: Optional[str] = Field(None, max_length=2000)
+    requirements: Optional[str] = Field(None, max_length=2000)
+    nice_to_have: Optional[str] = Field(None, max_length=2000)
+    responsibilities: Optional[str] = Field(None, max_length=2000)
+    other: Optional[str] = Field(None, max_length=1000)
+
+    class Config:
+        validate_assignment = True
+
 
 class JobDescriptionFields(BaseModel):
     job_title: str
